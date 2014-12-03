501e79360e50432d8ef623909f8d7c8761b11b65
Jonathan McCaffrey <Jon.McC_Social@Live.com>
2014-12-03 17:43:49 -0500

    Start of a grid based mobile game developed in Unity

---
+++ trunk/Assets/Scripts/DemoTank/Level/Tile.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Xml;
+
+public class Tile : MonoBehaviour {
+	
+	public enum EditorType { Tile, Button };
+	public enum CollidableType { Floor, Wall, Damage, Bullet, Enemy, Player, Breakable };
+	
+	public EditorType mEditorType = EditorType.Button;
+	public CollidableType mCollidableType = CollidableType.Floor;
+	
+	float mGridSize = 50.0f;
+	
+
+	public KeyCode mHotKey = KeyCode.None;
+
+	void TileToGrid () {
+		mGridSize = GetComponent<BoxCollider2D> ().size.x;
+		if (mEditorType == EditorType.Tile) {
+			Vector2 gridPosition = new Vector2 ((float)(((int)(this.transform.position.x / mGridSize)) * mGridSize) + mGridSize * 0.5f, (float)(((int)(this.transform.position.y / mGridSize)) * mGridSize) + mGridSize * 0.5f);
+			this.transform.position = gridPosition;
+		}
+	}
+	
+	public void Start() {
+		TileToGrid ();
+	}
+	
+	public void OnDrawGizmos () {
+		TileToGrid ();
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	public string Save() {
+
+		System.Text.StringBuilder stringBuilder = new System.Text.StringBuilder ();
+
+		using (XmlWriter writer = XmlWriter.Create(stringBuilder)) {
+			
+			writer.WriteElementString ("Position", this.gameObject.transform.position.ToString ());
+			writer.WriteElementString ("Scale", this.gameObject.transform.localScale.ToString());
+			writer.WriteElementString ("FileName", GetComponent<SpriteRenderer> ().sprite.texture.name);
+		
+		}
+
+		return stringBuilder.ToString();
+
+	}
+	
+	public void Load(XmlReader data) {
+	/*	using(XmlReader xmlReader = XmlReader.Create(data)) {
+			string position = xmlReader.ReadElementString("Position");
+			string scale = xmlReader.ReadElementString("Scale");
+			string fileName = xmlReader.ReadElementString("FileName");
+
+
+		}*/
+	}
+}
---
+++ trunk/Assets/Scripts/DemoTank/Systems/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	
+	public GameObject mainPlayer = null;
+
+	void FollowPlayerCamera () {
+		if (mainPlayer) {
+			gameObject.transform.position = new Vector3 (mainPlayer.transform.position.x, mainPlayer.transform.position.y, -500);
+		}
+	}
+	
+	void Update () {
+		FollowPlayerCamera ();
+	}
+}
